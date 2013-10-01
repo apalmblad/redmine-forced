a8d4ee09129b8a6265ae157daf47ee1ef13614ae
Adam Palmblad <adam.palmblad@teampages.com>
2013-10-01 13:26:08 -0700

    Fix up array issue causing only one query to be allowed.

--- lib/forced_hook_listener.rb
+++ lib/forced_hook_listener.rb
@@ -1,11 +1,12 @@
 module ForcedIssue
   # -------------------------------------------- check_for_forced_query_limits
   def check_for_forced_query_limits
-    Setting.plugin_forced['queries'].values.each do |data|
-      query = IssueQuery.find( :first, :conditions => ["id=?",data['query_id']] )
+    Setting.plugin_forced['query_ids'].each_with_index do |query_id, i|
+      query = IssueQuery.find( :first, :conditions => ["id=?",query_id] )
       next if query.project_id && query.project_id != project_id
-      if query.issue_count >= data['max_issues'].to_i
-        errors.add( :base, "You have too many issues in #{query.name}.  Please get that number below #{data['max_issues']} before adding more issues." )
+      max_issues = Setting.plugin_forced['max_issues'][i].to_i
+      if query.issue_count >= max_issues
+        errors.add( :base, "You have too many issues in #{query.name}.  Please get that number below #{max_issues} before adding more issues." )
         return false
       end
     end
@@ -18,11 +19,12 @@ module ForcedIssue
 end
 module ForceIssueControllerChanges
   def check_if_at_issue_limit
-    Setting.plugin_forced['queries'].values.each do |data|
-      query = IssueQuery.find( :first, :conditions => ["id=?",data['query_id']] )
+    Setting.plugin_forced['query_ids'].each_with_index do |query_id, i|
+      query = IssueQuery.find( :first, :conditions => ["id=?",query_id] )
       next if query.project_id && query.project_id != project_id
-      if query.issue_count >= data['max_issues'].to_i
-        flash[:error] = "You have too many issues in #{query.name}.  Please get that number below #{data['max_issues']} before adding more issues."
+      max_issues = Setting.plugin_forced['max_issues'][i].to_i
+      if query.issue_count >= max_issues
+        flash[:error] = "You have too many issues in #{query.name}.  Please get that number below #{max_issues} before adding more issues."
         redirect_to project_issues_url( params[:project_id], :query_id => query.id )
         return false
       end
