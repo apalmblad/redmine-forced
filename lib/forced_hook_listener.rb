d7451a3178102d298d9827873f74f128128e6e95
s19n <mailbox@s19n.net>
2015-04-21 14:42:28 +0200

    Should fix #1

--- lib/forced_hook_listener.rb
+++ lib/forced_hook_listener.rb
@@ -1,13 +1,15 @@
 module ForcedIssue
   # -------------------------------------------- check_for_forced_query_limits
   def check_for_forced_query_limits
-    Setting.plugin_forced['query_ids'].each_with_index do |query_id, i|
-      query = IssueQuery.find( :first, :conditions => ["id=?",query_id] )
-      next if query.project_id && query.project_id != project_id
-      max_issues = Setting.plugin_forced['max_issues'][i].to_i
-      if query.issue_count >= max_issues
-        errors.add( :base, "You have too many issues in #{query.name}.  Please get that number below #{max_issues} before adding more issues." )
-        return false
+    unless Setting.plugin_forced['query_ids'].nil?
+      Setting.plugin_forced['query_ids'].each_with_index do |query_id, i|
+        query = IssueQuery.find( :first, :conditions => ["id=?",query_id] )
+        next if query.project_id && query.project_id != project_id
+        max_issues = Setting.plugin_forced['max_issues'][i].to_i
+        if query.issue_count >= max_issues
+          errors.add( :base, "You have too many issues in #{query.name}.  Please get that number below #{max_issues} before adding more issues." )
+          return false
+        end
       end
     end
     return true
@@ -19,14 +21,16 @@ module ForcedIssue
 end
 module ForceIssueControllerChanges
   def check_if_at_issue_limit
-    Setting.plugin_forced['query_ids'].each_with_index do |query_id, i|
-      query = IssueQuery.find( :first, :conditions => ["id=?",query_id] )
-      next if query.project_id && query.project_id != project_id
-      max_issues = Setting.plugin_forced['max_issues'][i].to_i
-      if query.issue_count >= max_issues
-        flash[:error] = "You have too many issues in #{query.name}.  Please get that number below #{max_issues} before adding more issues."
-        redirect_to project_issues_url( params[:project_id], :query_id => query.id )
-        return false
+    unless Setting.plugin_forced['query_ids'].nil?
+      Setting.plugin_forced['query_ids'].each_with_index do |query_id, i|
+        query = IssueQuery.find( :first, :conditions => ["id=?",query_id] )
+        next if query.project_id && query.project_id != project_id
+        max_issues = Setting.plugin_forced['max_issues'][i].to_i
+        if query.issue_count >= max_issues
+          flash[:error] = "You have too many issues in #{query.name}.  Please get that number below #{max_issues} before adding more issues."
+          redirect_to project_issues_url( params[:project_id], :query_id => query.id )
+          return false
+        end
       end
     end
     return true
